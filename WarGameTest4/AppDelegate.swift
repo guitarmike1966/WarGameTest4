6620289afae52091e76431731981fba55803a00b
Mike O'Connell <guitarmike1966@gmail.com>
2020-05-08 20:03:53 -0500

    Initial Commit

---
+++ WarGameTest4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WarGameTest4
+//
+//  Created by Michael O'Connell on 5/8/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WarGameTest4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WarGameTest4
+//
+//  Created by Michael O'Connell on 5/8/20.
+//  Copyright © 2020 Michael O'Connell. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
